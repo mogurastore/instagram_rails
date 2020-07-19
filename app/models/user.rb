4a89994eac1b96addb3f2540542a8b6121c0ee8e
lisp719 <lisp719@gmail.com>
2020-07-19 16:16:08 +0900

    feat: フォロー機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :active_relationships, class_name: 'Follow', foreign_key: 'user_id'
+  has_many :passive_relationships, class_name: 'Follow', foreign_key: 'target_user_id'
+  has_many :followings, through: :active_relationships, source: :target_user
+  has_many :followers, through: :passive_relationships, source: :user
   has_many :photos
 end
