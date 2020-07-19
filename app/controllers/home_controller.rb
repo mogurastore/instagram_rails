71fb4116d882d15d2079860945bd663f5fccbe5a
lisp719 <lisp719@gmail.com>
2020-07-19 18:15:41 +0900

    feat: タイムラインを実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @feeds = Photo.where(user_id: [current_user.id, *current_user.following_ids]).order(created_at: :desc)
   end
 end
