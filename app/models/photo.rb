c30d4b5adf3e8a02b70179beb07177acf6d23e78
lisp719 <lisp719@gmail.com>
2020-08-08 14:08:16 +0900

    feat: 写真の削除を実装

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,8 +1,8 @@
 class Photo < ApplicationRecord
   belongs_to :user
 
-  has_many :comments
-  has_many :likes
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   has_one_attached :image
 
